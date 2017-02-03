91cde29a0a45802f3de41e266189125cbab292c8
Toshiba <Toshiba@LEIDY-OF19DR>
2017-02-03 08:33:49 -0500

    inicio_daniel

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-require_once("db/db.php");
-require_once("controllers/personas_controller.php");
+<html>
+    	<head>
+        	<meta charset="UTF-8">
+    	</head>
+    	<body>
+        	<?php
+        		require_once('controllers/index.php');
+        	?>
+                <?php
+                require_once("db/db.php");
+                require_once("controllers/personas_controller.php");
 ?>
+    	</body>
+</html>
