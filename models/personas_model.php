0a690e9a3b331088eab77f92000f2977cc7c9ad3
DENIS <DENIS@192.168.36.158>
2017-02-03 14:06:04 -0500

    conexion

--- models/personas_model.php
+++ models/personas_model.php
@@ -1,6 +1,6 @@
 <?php
-class personas_model{
-    private $db;
+class personas_model
+  { private $db;
     private $personas;
  
     public function __construct()
@@ -8,11 +8,12 @@ class personas_model{
         $this->personas=array();
     }
     public function get_personas()
-    {   $consulta=$this->db->query("select * from personas;");
+      { $consulta=$this->db->query("select * from personas;");
         while($filas=$consulta->fetch_assoc())
-        {$this->personas[]=$filas;
+        {    $this->personas[]=$filas;
         }
         return $this->personas;
-    }
-}
+    
+      }
+  }
 ?>
